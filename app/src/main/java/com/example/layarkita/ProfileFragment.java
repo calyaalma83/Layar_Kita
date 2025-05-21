2c171162c3c5ca9003a3368efd7a5573074b9cbb
calyaalma83 <kacangalmond83@gmail.com>
2025-05-21 14:42:03 +0700

    ...

--- app/src/main/java/com/example/layarkita/ProfileFragment.java
+++ app/src/main/java/com/example/layarkita/ProfileFragment.java
@@ -2,6 +2,7 @@ package com.example.layarkita;
 
 import android.app.AlertDialog;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -27,6 +28,12 @@ public class  ProfileFragment extends Fragment{
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        TextView tvAkun = view.findViewById(R.id.tvAkun);
+        tvAkun.setOnClickListener(v -> {
+            Intent intent = new Intent(getActivity(), AkunActivity.class);
+            startActivity(intent);
+        });
 
         TextView profileName = view.findViewById(R.id.profileName);
         TextView tvBahasa = view.findViewById(R.id.tvBahasa);
